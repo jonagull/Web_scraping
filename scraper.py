2c99634fb8670fbd639627da2cd850a65aab2eb7
unknown <jonathangullik@gmail.com>
2021-11-24 16:47:32 +0100

    Built as function, so you can now enter url and folder name in cli

--- scraper.py
+++ scraper.py
@@ -1,12 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
+import os
 
-def getdata(url):
+
+def imageDownload(url, folder):
+    try:
+        os.mkdir(os.path.join(os.getcwd(), folder))
+    except:
+        pass
+    
+    os.chdir(os.path.join(os.getcwd(), folder))
     r = requests.get(url)
-    return r.text
+    soup = BeautifulSoup(r.text, 'html.parser')
+    images = soup.find_all('img')
 
-htmldata = getdata("https://boards.4channel.org/g/")
-soup = BeautifulSoup(htmldata, 'html.parser')
-for item in soup.find_all('img'):
-    print(item['src'])
+    for image in images:
+        name = image['alt']
+        link = image['src']
+        with open(name.replace(' ', '-').replace('/', '').replace('!', '') + '.jpg', 'wb') as f:
+            im = requests.get(link)
+            f.write(im.content)
+            print("Writing: {}".format(name))
+
+a = input("Enter url: ")
+b = input("Enter folder name: ")
+
+imageDownload(a, b)
+
+
+
 
